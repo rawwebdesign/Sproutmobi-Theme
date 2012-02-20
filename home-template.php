e56b83221bcdee033371e426bd2cbac6bbb37500
Brad <rawwebdesign@gmail.com>
2012-02-19 20:42:16 -0500

    Fixed the blog page

---
+++ home-template.php
@@ -0,0 +1,222 @@
+<?php
+/*
+Template Name: Home Page Sales Page
+*/
+?>
+
+<?php get_header(); ?>
+
+<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+      <!-- Main hero unit for a primary marketing message or call to action -->
+      <div class="green-hero">
+        <div id='handofgod'>
+          <img src='http://www.sproutmobi.com/wp-content/uploads/2012/02/cutouthand.png'>
+        </div>
+        <div='leftsidethis'>
+          <div id='topline'>The <span class='special'>All-In-One</span> System for Creating <span class='special'>Simply Awesome</span></div>
+          <div id='bottomline'>Mobile Websites</div>
+          <!-- <p>The quick & easy way to create a mobile-optimized website.  Expand your website's reach and start embracing your mobile customers!</p> -->
+        </div>
+      </div>
+    
+    
+
+    <div class='fullwidth greybg'>
+          <p>Build a Free Mobile Website in Less than 30 Seconds: <a href='/sign-up'class="btn primary large">Start 30 Day Free Trial</a><!-- or <a class="btn large">Take the Tour</a>--></p>
+    </div>
+
+    <div class='container'>
+      <div class='row'>
+        <div class='span8'>
+          <h2>Build a Custom Mobile Website in Minutes</h2>
+          <p>The Mobile Loft website builder enables anyone to visually build a website for mobile devices.  Now your website will look great and make more money by tapping into millions of cell-phone users!</p>
+
+          <p>Join the movement & build a mobile website with airtight SEO, incredible design flexibility, and lightning-fast loading times that Mobile Loft provides.</p>
+
+          <p>Start your risk-free trial now, and enjoy your mobile website for 30 days before you pay anything.</p>
+
+          <br><a href='/sign-up'class="btn primary large">Start Building for Free</a> or <A href='http://www.sproutmobi.com/why-do-i-need-a-mobile-website/'>learn why you need a mobile website...</a></br>
+        </div>
+        <div class='span8'>
+          <!-- Then somewhere in the <body> section -->
+          <div id="slider" class="nivoSlider">
+              <img src="/timthumb/timthumb.php?src=http://www.sproutmobi.com/wp-content/uploads/2011/10/front.jpg&q=100&w=450" alt="" />
+              <img src="/timthumb/timthumb.php?src=http://www.sproutmobi.com/wp-content/uploads/2011/10/hours.jpg&q=100&w=450" alt="" />
+              <img src="/timthumb/timthumb.php?src=http://www.sproutmobi.com/wp-content/uploads/2011/10/map.jpg&q=100&w=450" alt="" />
+              <img src="/timthumb/timthumb.php?src=http://www.sproutmobi.com/wp-content/uploads/2011/10/coupon.jpg&q=100&w=450" alt="" />
+          </div>
+        </div>
+      </div>
+
+      <!-- end of first row -->
+
+      <hr>
+        <div class='quotey'>
+          <h3>Why You'll Love SproutMobi</h3>
+        </div>
+      <hr>
+
+        <div class='row minheightfeature'>
+          <div class='span4'>
+            <ul class="media-grid">
+              <li>
+                <a rel="shadowbox" href="http://www.sproutmobi.com/wp-content/uploads/2011/11/Screen-shot-2011-11-24-at-1.36.52-PM.png">
+                  <img class="thumbnail" src="/timthumb/timthumb.php?src=http://www.sproutmobi.com/wp-content/uploads/2011/11/Screen-shot-2011-11-24-at-1.36.52-PM.png&q=100&w=210&h=150" alt="">
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div class='span12'>
+            <h2>Anyone Can Do It</h2>
+            <p>No coding required.  Your simple dashboard allows you to build a mobile website in just minutes.  Your website can be as simple or customized as you want it to be!</p>
+          </div>
+        </div>
+        <div class='row minheightfeature'>
+          <div class='span4'>
+            <ul class="media-grid">
+              <li>
+                <a rel="shadowbox" href="http://www.sproutmobi.com/wp-content/uploads/2011/11/Screen-shot-2011-11-24-at-1.37.22-PM.png">
+                  <img class="thumbnail" src="/timthumb/timthumb.php?src=http://www.sproutmobi.com/wp-content/uploads/2011/11/Screen-shot-2011-11-24-at-1.37.22-PM.png&q=100&w=210&h=150" alt="">
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div class='span12'>
+            <h2>Step by Step</h2>
+            <p>Not sure what you want out of a mobile website?  Follow the step-by-step wizard to create a cutting-edge website your on-the-go users will love.  It's as easy to pointing and clicking!</p>
+          </div>
+        </div>
+        <div class='row minheightfeature'>
+          <div class='span4'>
+            <ul class="media-grid">
+              <li>
+                <a rel="shadowbox" href="http://www.sproutmobi.com/wp-content/uploads/2011/11/Screen-shot-2011-11-24-at-1.37.43-PM.png">
+                  <img class="thumbnail" src="/timthumb/timthumb.php?src=http://www.sproutmobi.com/wp-content/uploads/2011/11/Screen-shot-2011-11-24-at-1.37.43-PM.png&q=100&w=210&h=150" alt="">
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div class='span12'>
+            <h2>Unlimited Pages & Free Access to Every Feature</h2>
+            <p>Unlike many mobile site builders, there is no limit or upcharge for access to all of our features.  Once you are a member, you can make the mobile website of your dreams for one low price.</p>
+          </div>
+        </div>
+        <div class='row minheightfeature'>
+          <div class='span4'>
+            <ul class="media-grid">
+              <li>
+                <a rel="shadowbox" href="http://www.sproutmobi.com/wp-content/uploads/2011/11/Screen-shot-2011-11-24-at-1.37.55-PM.png">
+                  <img class="thumbnail" src="/timthumb/timthumb.php?src=http://www.sproutmobi.com/wp-content/uploads/2011/11/Screen-shot-2011-11-24-at-1.37.55-PM.png&q=100&w=210&h=150" alt="">
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div class='span12'>
+            <h2>Add Logos and Images</h2>
+            <p>A picture is worth a thousand words, but how do you make your images ready for the mobile web? Never worry again- just upload your images and they'll be sized and shrunk to fit your mobile phone users.</p>
+          </div>
+        </div>
+        <div class='row minheightfeature'>
+          <div class='span4'>
+            <ul class="media-grid">
+              <li>
+                <a rel="shadowbox" href="http://www.sproutmobi.com/wp-content/uploads/2011/11/Screen-shot-2011-11-24-at-1.38.08-PM.png">
+                  <img class="thumbnail" src="/timthumb/timthumb.php?src=http://www.sproutmobi.com/wp-content/uploads/2011/11/Screen-shot-2011-11-24-at-1.38.08-PM.png&q=100&w=210&h=150" alt="">
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div class='span12'>
+            <h2>Use Our Template or Make Your Own</h2>
+            <p>Want your mobile website to stand out from the crowd?  Choose a template and customize it, or start from scratch.  Build your own mobile site in less than ten minutes!</p>
+          </div>
+        </div>
+        <div class='row minheightfeature'>
+          <div class='span4'>
+            <ul class="media-grid">
+              <li>
+                <a rel="shadowbox" href="http://www.sproutmobi.com/wp-content/uploads/2011/11/Screen-shot-2011-11-24-at-1.38.47-PM.png">
+                  <img class="thumbnail" src="/timthumb/timthumb.php?src=http://www.sproutmobi.com/wp-content/uploads/2011/11/Screen-shot-2011-11-24-at-1.38.47-PM.png&q=100&w=210&h=150" alt="">
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div class='span12'>
+            <h2>Unlimited Customer Service</h2>
+            <p>Never feel confused again! Passionate mobile website builders are waiting to answer your questions any time. Let us help you build the mobile website of your dreams.</p>
+          </div>
+        </div>
+        <div class='row minheightfeature'>
+          <div class='span4'>
+            <ul class="media-grid">
+              <li>
+                <a rel="shadowbox" href="http://www.sproutmobi.com/wp-content/uploads/2011/11/Screen-shot-2011-11-24-at-1.39.12-PM.png">
+                  <img class="thumbnail" src="/timthumb/timthumb.php?src=http://www.sproutmobi.com/wp-content/uploads/2011/11/Screen-shot-2011-11-24-at-1.39.12-PM.png&q=100&w=210&h=150" alt="">
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div class='span12'>
+            <h2>Use Your Own URL</h2>
+            <p>Whether you want the ease of using the default URL or hooking your website up to your own URL, you'll have the options and support to fit your needs. We offer the most advanced CNAME capabilities for the tech-savvy.  If you don't know what a CNAME is... don't worry, we've got you covered too.</p>
+          </div>
+        </div>
+        <div class='row minheightfeature'>
+          <div class='span4'>
+            <ul class="media-grid">
+              <li>
+                <a rel="shadowbox" href="http://www.sproutmobi.com/wp-content/uploads/2011/11/Screen-shot-2011-11-24-at-1.39.44-PM.png">
+                  <img class="thumbnail" src="/timthumb/timthumb.php?src=http://www.sproutmobi.com/wp-content/uploads/2011/11/Screen-shot-2011-11-24-at-1.39.44-PM.png&q=100&w=210&h=150" alt="">
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div class='span12'>
+            <h2>No Coding Required!</h2>
+            <p>Not a coder?  That't perfectly fine.  If you can point and click, you can make your own mobile website.  </p>
+          </div>
+        </div>
+        <div class='row minheightfeature'>
+          <div class='span4'>
+            <ul class="media-grid">
+              <li>
+                <a rel="shadowbox" href="http://www.sproutmobi.com/wp-content/uploads/2011/11/Screen-shot-2011-11-24-at-2.45.53-PM.png">
+                  <img class="thumbnail" src="/timthumb/timthumb.php?src=http://www.sproutmobi.com/wp-content/uploads/2011/11/Screen-shot-2011-11-24-at-2.45.53-PM.png&q=100&w=210&h=150" alt="">
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div class='span12'>
+            <h2>Match Your Existing Website</h2>
+            <p>If you want your mobile website to be a seamless transition from your existing site, use your simple customization page to match the colors exactly.  Your mobile website will be a better, faster, and smaller version of your desktop site in only minutes!</p>
+          </div>
+        </div>
+        <div class='row minheightfeature'>
+          <div class='span4'>
+            <ul class="media-grid">
+              <li>
+                <a rel="shadowbox" href="http://www.sproutmobi.com/wp-content/uploads/2011/12/Screen-Shot-2011-12-27-at-10.09.28-PM.png">
+                  <img class="thumbnail" src="/timthumb/timthumb.php?src=http://www.sproutmobi.com/wp-content/uploads/2011/12/Screen-Shot-2011-12-27-at-10.09.28-PM.png&q=100&w=210&h=150" alt="">
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div class='span12'>
+            <h2>WordPress Plugin</h2>
+            <p>Do you have a WordPress website?  The WordPress plugin makes it dead simple to add Mobile Loft to your website!</p>
+          </div>
+        </div>
+
+     
+      </div>
+
+    </div>
+
+
+</div><!-- end post wrapper -->
+<div class='fullwidth greybg'>
+          <p>Build a Free Mobile Website in Less than 30 Seconds: <a href='/sign-up'class="btn primary large">Start 30 Day Free Trial</a><!-- or <a class="btn large">Take the Tour</a>--></p>
+</div>
+      
+<?php get_footer(); ?>
--- index.php
+++ index.php
@@ -1,39 +1,62 @@
 <?php get_header(); ?>
 
-  <?php while ( have_posts() ) : the_post() ?>
     <div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
       
       <div class='container index'>
 
-      <div id="sidebar">
-        <div class='span4'>
-          <?php if ( !function_exists('dynamic_sidebar')
-                  || !dynamic_sidebar() ) : ?>
-           <div class="title">About</div>
-           <p>This is my blog.</p>
-           <div class="title">Links</div>
-           <ul>
-            <li><a href="http://example.com">Example</a></li>
-           </ul>
-          <?php endif; ?>
+        <div id="sidebar">
+          <div class='span4'>
+            <?php if ( !function_exists('dynamic_sidebar')
+                    || !dynamic_sidebar() ) : ?>
+             <div class="title">About</div>
+             <p>This is my blog.</p>
+             <div class="title">Links</div>
+             <ul>
+              <li><a href="http://example.com">Example</a></li>
+             </ul>
+            <?php endif; ?>
+          </div>
         </div>
-      </div>
 
-      <div class='row'>
-        <div class='span12'>
-          <h2 class="entry-title"><a href="<?php the_permalink(); ?>" title="<?php printf( __('Permalink to %s', 'your-theme'), the_title_attribute('echo=0') ); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
-   
-          <div class="entry-content">
-            <?php the_content( __( 'Continue reading <span class="meta-nav">&raquo;</span>', 'your-theme' )  ); ?>
-            <?php wp_link_pages('before=<div class="page-link">' . __( 'Pages:', 'your-theme' ) . '&after=</div>') ?>
-          </div><!-- .entry-content -->
-        </div>
-      </div>
+        <div class='row'>
+          <div class='span12'>
+            <?php if(have_posts()) : while(have_posts()) : the_post(); ?>
+
+              <div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+                <?php the_title('<h2 class="entry-title"><a href="' . get_permalink() . '" title="' . the_title_attribute('echo=0') . '" rel="bookmark">', '</a></h2>'); ?>
+
+                <p class="byline">
+                  <span class="author vcard"><?php the_author_posts_link(); ?></span> <span class="sep">|</span> 
+                  <abbr class="published" title="<?php the_time(__('l, F jS, Y, g:i a', 'example')); ?>"><?php the_time(__('F j, Y', 'example')); ?></abbr>
+                  <?php edit_post_link(__('Edit', 'example'), ' <span class="sep">|</span> <span class="edit">', '</span> '); ?>
+                </p>
+
+                <div class="entry-content">
+                  <?php the_content(__('Continue reading', 'example')); ?>
+                  <?php wp_link_pages('before=<p class="pages">' . __('Pages:','example') . '&after=</p>'); ?>
+                </div>
+
+                <p class="entry-meta">
+                  <span class="categories"><?php _e('Posted in', 'example'); ?> <?php the_category(', '); ?></span>
+                  <?php the_tags('<span class="tags"> <span class="sep">|</span> ' . __('Tagged', 'example') . ' ', ', ', '</span>'); ?> 
+                  <span class="sep">|</span> <?php comments_popup_link(__('Leave a response', 'example'), __('1 Response', 'example'), __('% Responses', 'example'), 'comments-link', __('Comments closed', 'example')); ?> 
+                </p>
+
+              </div>
+
+              <?php endwhile; ?>
+
+            <?php else : ?>
+
+              <p class="no-posts"><?php _e('Sorry, no posts matched your criteria', 'example'); ?></p>
+
+            <?php endif; ?> 
+          </div><!-- span12 -->
+        </div><!-- row -->
 
       </div> <!-- /container -->
                     
     </div><!-- #post-<?php the_ID(); ?> --> 
-   
-  <?php endwhile; ?>
       
 <?php get_footer(); ?>
